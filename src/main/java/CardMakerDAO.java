56baa009a2dfcb12b4321d76dae36817decf98d8
Akim <proakim@gmail.com>
2019-11-04 22:17:42 -0600

    initial commit

---
+++ src/main/java/CardMakerDAO.java
@@ -0,0 +1,145 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.sql.ResultSetMetaData;
+
+public class CardMakerDAO {
+	
+	java.sql.Connection conn;
+	
+	public CardMakerDAO (){
+		try  {
+    		conn = DBUtils.connect();
+    	} catch (Exception e) {
+    		conn = null;
+    	}
+	}
+	
+	public int getRecipientID(String rName, String rEmail) throws Exception {
+        int recipientID = 0;
+		PreparedStatement ps = conn.prepareStatement("SELECT recipientID FROM cs509db.recipient where recipientName = '" + rName + "' and"
+        		+ " recipientEmail = '" + rEmail + "';");	    
+        ResultSet resultSet = ps.executeQuery();	
+        while (resultSet.next()) {
+        	recipientID = resultSet.getInt("recipientID");
+        }
+        resultSet.close();
+        ps.close();
+        return recipientID;
+	}
+	
+	public String createCard(String xOrient, String eType, String rName, String rEmail) throws Exception {
+		try {
+			PreparedStatement ps = conn.prepareStatement("Insert into cs509db.card values(?,?,?,?)");
+			ps.setNull(1,0);
+			ps.setInt (2, Integer.parseInt(xOrient));
+			ps.setInt(3, Integer.parseInt(eType));
+			ps.setInt(4, getRecipientID(rName, rEmail));			
+			ps.execute();
+			ps.close();
+			return "Card created.";
+		} catch (Exception ex) {
+			throw new Exception("Failed to create card." + ex.getMessage());
+		}
+	}
+	
+	public String deleteCard(String rName, String eType, String rEmail) throws Exception {
+		try {
+			PreparedStatement ps = 
+				conn.prepareStatement("delete from cs509db.card where recipientID = " + getRecipientID(rName, rEmail) + " and eventTypeID = " + 
+			Integer.parseInt(eType) + ";");
+				ps.execute();
+				ps.close();
+			return "Card deleted.";
+		} catch (Exception ex) {
+			throw new Exception("Failed to delete card." + ex.getMessage());
+		}
+	}
+	
+	public ArrayList<HashMap<String, String>> listAllRecipients() throws Exception {
+		 try {
+	            ArrayList<HashMap<String, String>> record = new ArrayList<HashMap<String, String>>();
+	            PreparedStatement ps = conn.prepareStatement("SELECT * FROM cs509db.recipient;");	    
+	            ResultSet resultSet = ps.executeQuery();	
+	            ResultSetMetaData rsMetaData = resultSet.getMetaData();
+	            while (resultSet.next()) {
+	            	HashMap<String, String> resultsMap = new HashMap<String,String>();
+	            	resultsMap.put(rsMetaData.getColumnName(0), resultSet.getString("recipientID"));
+	            	resultsMap.put(rsMetaData.getColumnName(1), resultSet.getString("recipientName"));
+	            	resultsMap.put(rsMetaData.getColumnName(2), resultSet.getString("recipientSurname"));	
+	            	resultsMap.put(rsMetaData.getColumnName(3), resultSet.getString("recipientEmail"));
+	            	record.add(resultsMap);
+	            }
+	            resultSet.close();
+	            ps.close();
+	            return record;
+	        } catch (Exception e) {
+	        	throw new Exception("Failed to list recipients " + e.getMessage());
+	        }
+	}
+	
+	public ArrayList<HashMap<String, String>> listAllCards() throws Exception {
+		 try {  
+	            ArrayList<HashMap<String, String>> record = new ArrayList<HashMap<String, String>>();
+	            PreparedStatement ps = conn.prepareStatement(
+	            		"Select cardID, cardOrientation, event, recipientName, recipientSurname, recipientEmail from card " + 
+	            		"inner join recipient on card.recipientID = recipient.recipientID " + 
+	            		"inner join event on card.eventTypeID = event.eventTypeID;"
+	            		);	    
+	            ResultSet resultSet = ps.executeQuery();	
+	            ResultSetMetaData rsMetaData = resultSet.getMetaData();
+	            while (resultSet.next()) {
+	            	HashMap<String, String> resultsMap = new HashMap<String,String>();
+	            	resultsMap.put(rsMetaData.getColumnName(1), resultSet.getString("cardID"));
+	            	resultsMap.put(rsMetaData.getColumnName(2), resultSet.getString("cardOrientation"));
+	            	resultsMap.put(rsMetaData.getColumnName(3), resultSet.getString("event"));	
+	            	resultsMap.put(rsMetaData.getColumnName(4), resultSet.getString("recipientName"));
+	            	resultsMap.put(rsMetaData.getColumnName(5), resultSet.getString("recipientSurname"));	
+	            	resultsMap.put(rsMetaData.getColumnName(6), resultSet.getString("recipientEmail"));
+	            	record.add(resultsMap);
+	            }
+	            resultSet.close();
+	            ps.close();
+	            return record;
+	        } catch (Exception e) {
+	        	throw new Exception("Failed to list recipients " + e.getMessage());
+	        }
+	}
+	
+	public void addRecipient() {
+		
+	}
+	
+	public void deleteRecipient() {
+		
+	}
+	
+	public void updateRecipient() {
+		
+	}
+	
+	public void addTextElement() {
+		
+	}
+	
+	public void deleteTextElement() {
+		
+	}
+
+	public void updateTextElement() {
+		
+	}
+	
+	public void addImageElement() {
+		
+	}
+	
+	public void updateImageElement() {
+		
+	}
+	
+	public void deleteImageElement() {
+		
+	}
+}
---
+++ src/main/java/DeleteCardHandler.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.Gson;
+
+public class DeleteCardHandler implements RequestStreamHandler {
+
+	ParseJSON parserUtils;
+	Map<String, String> parsedValues;
+	CardMakerDAO dao;
+	JSONUtils myUtils;
+	
+	public DeleteCardHandler() {
+		parserUtils = new ParseJSON();
+		parsedValues = new HashMap<String, String>();
+		dao = new CardMakerDAO();
+		myUtils = new JSONUtils();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void formatResponse(String jsonString, int statusCode) {
+        myUtils.getResponseJson().put("body", jsonString);  
+        myUtils.getResponseJson().put("statusCode", statusCode);
+	}
+	
+	@Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+		// extract body from incoming HTTP POST request. If any error, then return 422 error
+		try {
+	        parsedValues = parserUtils.jsonParser(input);
+		} catch (Exception pe) {
+			this.formatResponse(new Gson().toJson("Unable to process input"), 422);		
+		}
+
+		try {
+			String rName = parsedValues.get("recipientName");
+			String eType = parsedValues.get("eventType");
+			String rEmail = parsedValues.get("recipientEmail");
+			this.formatResponse(new Gson().toJson(dao.deleteCard(rName, eType, rEmail)), 200);
+		} catch (Exception e) {
+			this.formatResponse(new Gson().toJson(e.getMessage()), 400);
+		}
+	
+		OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8"); 
+		writer.write(myUtils.getResponseJson().toJSONString());  
+		writer.close();		
+	}
+}

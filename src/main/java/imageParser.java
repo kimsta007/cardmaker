bb1a5ef7366e080eca6e948402445042630773c0
Akim <proakim@gmail.com>
2019-11-17 19:50:11 -0600

    new file:   src/main/java/GetCardHandler.java 	new file:   src/main/java/imageParser.java 	new file:   target/classes/GetCardHandler.class 	new file:   target/classes/imageParser.class 	new file:   target/lib/commons-codec-1.9.jar

---
+++ src/main/java/imageParser.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
+
+public class imageParser {
+	
+	public static BufferedImage decodeToImage(String imageString) {
+        BufferedImage image = null;
+        try {
+        	byte[] imageByte = Base64.getDecoder().decode(imageString);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(imageByte);
+            image = ImageIO.read(inputStream);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return image; //image to be uploaded to S3 bucket
+    }
+	
+	public static String encodeToString(BufferedImage image, String type) {
+        String imageString = null;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(image, type, outputStream);
+            byte[] imageBytes = outputStream.toByteArray();
+            imageString = new String(Base64.getEncoder().encode(imageBytes));
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return imageString; //Return String to Client
+    }
+}

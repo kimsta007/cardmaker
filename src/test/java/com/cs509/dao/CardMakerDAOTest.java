9f40c22c91e636e03f0228060459b7ee47e9cf24
Akim <proakim@gmail.com>
2019-11-18 18:39:15 -0600

    new file:   src/test/java/com/cs509/dao/CardMakerDAOTest.java

---
+++ src/test/java/com/cs509/dao/CardMakerDAOTest.java
@@ -0,0 +1,14 @@
+package com.cs509.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardMakerDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

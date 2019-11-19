335b9a9ca54fbfc8caca0eb3dbfba264df4e8edc
Akim <proakim@gmail.com>
2019-11-18 18:53:43 -0600

    new file:   src/test/java/com/cs509/handlers/AddTextElementHandlerTest.java 	new file:   src/test/java/com/cs509/utils/ParseJSONTest.java 	deleted:    target/awslambda.com-1.0-SNAPSHOT.jar 	deleted:    target/awslambda.com-aws-lambda-package.zip 	deleted:    target/lib/FastInfoset-1.2.15.jar 	deleted:    target/lib/antlr-2.7.7.jar 	deleted:    target/lib/aws-java-sdk-core-1.11.664.jar 	deleted:    target/lib/aws-java-sdk-lambda-1.11.664.jar 	deleted:    target/lib/aws-lambda-java-core-1.1.0.jar 	deleted:    target/lib/byte-buddy-1.10.2.jar 	deleted:    target/lib/classmate-1.3.4.jar 	deleted:    target/lib/commons-codec-1.9.jar 	deleted:    target/lib/commons-logging-1.1.3.jar 	deleted:    target/lib/dom4j-2.1.1.jar 	deleted:    target/lib/gson-2.8.6.jar deleted:    target/lib/hamcrest-core-1.1.jar 	deleted:    target/lib/hibernate-commons-annotations-5.1.0.Final.jar 	deleted:    target/lib/hibernate-core-5.4.7.Final.jar 	deleted:    target/lib/httpclient-4.5.9.jar 	deleted:    target/lib/httpcore-4.4.11.jar 	deleted:    target/lib/ion-java-1.0.2.jar 	deleted:    target/lib/istack-commons-runtime-3.0.7.jar 	deleted:    target/lib/jackson-annotations-2.6.0.jar 	deleted:    target/lib/jackson-core-2.6.7.jar 	deleted:    target/lib/jackson-databind-2.6.7.3.jar 	deleted:    target/lib/jackson-dataformat-cbor-2.6.7.jar 	deleted:    target/lib/jandex-2.0.5.Final.jar 	deleted:    target/lib/javassist-3.24.0-GA.jar 	deleted:    target/lib/javax.activation-api-1.2.0.jar 	deleted:    target/lib/javax.persistence-api-2.2.jar 	deleted:    target/lib/jaxb-api-2.3.1.jar 	deleted:    target/lib/jaxb-runtime-2.3.1.jar 	deleted:    target/lib/jboss-logging-3.3.2.Final.jar 	deleted:    target/lib/jboss-transaction-api_1.2_spec-1.1.1.Final.jar 	deleted:    target/lib/jmespath-java-1.11.664.jar 	deleted:    target/lib/joda-time-2.8.1.jar 	deleted:    target/lib/json-simple-1.1.1.jar 	deleted:    target/lib/junit-4.10.jar 	deleted:    target/lib/mysql-connector-java-8.0.18.jar 	deleted:    target/lib/protobuf-java-3.6.1.jar 	deleted:    target/lib/stax-ex-1.8.jar 	deleted:    target/lib/txw2-2.3.1.jar 	deleted:    target/maven-archiver/pom.properties 	modified:   target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst

---
+++ src/test/java/com/cs509/handlers/AddTextElementHandlerTest.java
@@ -0,0 +1,14 @@
+package com.cs509.handlers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddTextElementHandlerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/cs509/utils/ParseJSONTest.java
@@ -0,0 +1,14 @@
+package com.cs509.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParseJSONTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
